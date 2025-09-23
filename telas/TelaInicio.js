5a547ef6d2a3d6e86fd22e028570280c30fda27b
AlvesssGG <alvesssfg2007@gmail.com>
2025-09-23 20:35:37 -0300

    criação de telas

---
+++ telas/TelaInicio.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function TelaInicio() {
+    return (
+        <View style={{ flex: 1, justifyContent:'center',alignItems: 'center'}}>
+            <Text>Tela de Inicio</Text>
+        </View>
+        
+    );
+}
+export default TelaInicio;
---
+++ telas/TelaSobre.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function TelaSobre() {
+    return (
+        <View style={{ flex: 1, justifyContent:'center',alignItems: 'center'}}>
+            <Text>Tela Sobre</Text>
+        </View>
+        
+    );
+}
+export default TelaSobre;
